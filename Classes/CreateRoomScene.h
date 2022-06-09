51dfecdd8a96201fe5b659403703cd69e656defe
Killerduckk <103769914+Killerduckk@users.noreply.github.com>
2022-06-09 14:16:57 +0800

    wgy，room的美化

--- Classes/CreateRoomScene.h
+++ Classes/CreateRoomScene.h
@@ -4,9 +4,12 @@
 #include"Client.h"
 #include "cocos2d.h"
 #include "ui/CocosGUI.h"
-
 USING_NS_CC;
 using namespace cocos2d::ui;
+
+#define Pos1 Vec2(300, 630) 
+#define Pos2 Vec2(1080, 450) 
+
 
 class CreateRoomScene : public cocos2d::Layer
 {
@@ -17,15 +20,15 @@ public:
 
 	void sendInvitionCallBack(Ref* r);
 
-	void toFirstPlayer(Ref* r);
-	void toSecondPlayer(Ref* r);
-
+	void toFirstPlayer(Ref* r) ;
+	void toSecondPlayer(Ref* r) ;
+	void aiPlayerCallBack(Ref* r);
 
 	ui::EditBox* IDQueryEdixBox;
 	Label* inviteFriendStatusLabel;
 
 	void findCallBack(Ref* r);
-
+	
 	static bool isInvited;
 	void updateGetInvited(float dt);
 
@@ -36,13 +39,17 @@ public:
 	CREATE_FUNC(CreateRoomScene);
 
 	//1p、2p的背景
-	Sprite* firstPlayer = Sprite::create("elements//1pbg.png");
-	Sprite* secondPlayer = Sprite::create("elements//2pbg.png");
+	Sprite* myPlayer = Sprite::create("player1.png");
+	Sprite* enemyPlayer = Sprite::create("player2.png");
+	Sprite* AIPlayer=Sprite::create("AI.png");
+	bool myPlayerAdded = false;
+	bool enemyPlayerAdded = false;
+	bool AIPlayerAdded = false;
 	//1p、2p的标签
 	Label* myPlayerLabel = Label::create();
 	Label* friendPlayerLabel = Label::create();
 	//提示信息，是否找到好友
-	Label* findFriendStatusLabel = Label::create();
+	Label* findFriendStatusLabel=Label::create();
 	static int myNum;
 	static int friendNum;
 	static int AIorPerson;
